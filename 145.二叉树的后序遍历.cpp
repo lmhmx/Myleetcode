ae846af0d1006a60af1d748a21cbe1bf97507023
lmhmx <liming18739796090@gmail.com>
2021-11-22 19:22:19 +0800

    后序遍历，迭代版

--- 145.二叉树的后序遍历.cpp
+++ 145.二叉树的后序遍历.cpp
@@ -24,20 +24,65 @@ class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        postorderTraversal(root, ans);
+        list<TreeNode*> nodes;
+        TreeNode* p;
+        stack<bool> left_visited;
+        stack<bool> right_visited;
+        if(root!=nullptr)
+        {
+            nodes.push_back(root);
+            left_visited.push(false);
+            right_visited.push(false);
+        }
+        while(!nodes.empty())
+        {
+            p = nodes.front();
+            if(p->right!=nullptr && !right_visited.top())
+            {
+                nodes.push_front(p->right);
+                right_visited.pop();
+                right_visited.push(true);
+                if(p->left!=nullptr && !left_visited.top())
+                {
+                    nodes.push_front(p->left);
+                    left_visited.pop();
+                    left_visited.push(true);
+                    left_visited.push(false);
+                    right_visited.push(false);
+                }
+                left_visited.push(false);
+                right_visited.push(false);
+                
+            }
+            else if(p->left!=nullptr && !left_visited.top())
+            {
+                nodes.push_front(p->left);
+                left_visited.pop();
+                left_visited.push(true);
+                left_visited.push(false);
+                right_visited.push(false);
+            }
+            else
+            {
+                ans.push_back(p->val);
+                nodes.pop_front();
+                left_visited.pop();
+                right_visited.pop();
+            }  
+        }
         return ans;
     }
-private:
-    void postorderTraversal(TreeNode* node, vector<int>& ans)
-    {
-        if(node==nullptr)
-        {
-            return;
-        }
-        postorderTraversal(node->left, ans);
-        postorderTraversal(node->right, ans);
-        ans.push_back(node->val);
-    }
 };
+int main()
+{
+    Solution s;
+    TreeNode* a= new TreeNode(1);
+    TreeNode* b= new TreeNode(2);
+    TreeNode* c= new TreeNode(3);
+    c->left = a;
+    c->right = b;
+    s.postorderTraversal(c);
+    return 0;
+}
 // @lc code=end
 
