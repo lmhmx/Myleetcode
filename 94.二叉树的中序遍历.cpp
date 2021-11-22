737a13c6f2a85c57e3c00867c3eeecffac09d523
lmhmx <liming18739796090@gmail.com>
2021-11-22 18:42:34 +0800

    中序遍历

---
+++ 94.二叉树的中序遍历.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+
+# include<vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorderTraversal(ans, root);
+        return ans;
+    }
+private:
+    void inorderTraversal(vector<int>& a, TreeNode* root)
+    {
+        if(root==nullptr)
+        {
+            return ;
+        }
+        inorderTraversal(a,root->left);
+        a.push_back(root->val);
+        inorderTraversal(a,root->right);
+
+    }
+};
+// @lc code=end
+
