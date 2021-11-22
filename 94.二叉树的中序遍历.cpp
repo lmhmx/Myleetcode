6894f2ebc5bbb6f873b81d205b377217655d2880
lmhmx <liming18739796090@gmail.com>
2021-11-22 18:58:41 +0800

    迭代版中序遍历

--- 94.二叉树的中序遍历.cpp
+++ 94.二叉树的中序遍历.cpp
@@ -7,6 +7,8 @@
 // @lc code=start
 
 # include<vector>
+# include <stack>
+# include <list>
 using namespace std;
 struct TreeNode {
     int val;
@@ -21,20 +23,39 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorderTraversal(ans, root);
-        return ans;
-    }
-private:
-    void inorderTraversal(vector<int>& a, TreeNode* root)
-    {
-        if(root==nullptr)
+        list<TreeNode*> nodes;
+        TreeNode* p;
+        stack<bool> left_visited;
+        if(root!=nullptr)
         {
-            return ;
+            nodes.push_front(root);
+            left_visited.push(false);
         }
-        inorderTraversal(a,root->left);
-        a.push_back(root->val);
-        inorderTraversal(a,root->right);
+        
+        while(!nodes.empty())
+        {
+            p = nodes.front();
+            if(p->left!=nullptr && !left_visited.top())
+            {
+                nodes.push_front(p->left);
+                left_visited.pop();
+                left_visited.push(true);
+                left_visited.push(false);
+            }
+            else
+            {
+                ans.push_back(p->val);
+                left_visited.pop();
+                nodes.pop_front();
+                if(p->right!=nullptr)
+                {
+                    nodes.push_front(p->right);
+                    left_visited.push(false);
+                }
 
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
