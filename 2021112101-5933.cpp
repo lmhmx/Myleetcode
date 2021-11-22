5c603f6307364d774b027d48c5754c82704a4aef
lmhmx <liming18739796090@gmail.com>
2021-11-22 11:37:51 +0800

    超时了

--- 2021112101-5933.cpp
+++ 2021112101-5933.cpp
@@ -1,47 +1,146 @@
 # include <vector>
+# include <cmath>
+# include <string>
+# include <algorithm>
 using namespace std;
 class Solution {
 public:
     long long kMirror(int k, int n) {
-        long long n1 = 1;
-        long long n2 = 1;
+        long long a = 0;
+        long long b = 0;
         long long ans = 0;
-        long long size = 0;
-        long long num1 = 0;
-        long long num2 = 0;
+        int size = 0;
+
+        a = getNext(a, 10);
+        b = getNext(b, k);
+            
         while(size < n)
         {
-            num1=getKn(k, n1);
-            num2=getKn(k, n2);
-            if(num1==num2)
+            
+            if(a==b)
             {
-                ans+=num1;
+                ans=ans+a;
                 size++;
+                a = getNext(a, 10);
+                b = getNext(b, k);
             }
-            else if(num1<num2)
+            else if(a<b)
             {
-                n1++;
+                a=getNext(a, 10);
             }
             else
             {
-                n2++;
+                b= getNext(b, k);
             }
         }
         return ans;
     }
 private:
-    long long getKn(int k, long long n)
+    long long getNext(long long n, int k)
     {
-        if(n<=k-1)
+        long long num = n;
+        long long ans;
+        bool all_k_1 = true;
+        vector<int> nums;
+        if(num ==0)
         {
-            return k-1;
+            all_k_1 = false;
+            nums.push_back(0);
         }
-        n=n-(k-1);
-        if(n<=(k-1))
+        else
         {
-            return k*n + n;
+            while(num!=0)
+            {
+                nums.push_back(num%k);
+                if(num%k!=k-1)
+                {
+                    all_k_1 = false;
+                }
+                num=num/k;
+            }
         }
-        n=n-(k-1);
-        if(n<=)
+        if(all_k_1)
+        {
+            ans = n+2;
+        }
+        else{
+            if(nums.size()%2==1)
+            {
+                if(nums[nums.size()/2] !=k-1)
+                {
+                    ans = n + pow(k,nums.size()/2);
+                }
+                else
+                {
+                    int tmp = 0;
+                    for(int i=0;i<nums.size()/2;i++)
+                    {
+                        tmp = tmp*k+nums[i];
+                    }
+                    tmp = tmp+1;
+                    vector<int> tmp_v = toVector(tmp, k);
+                    reverse(tmp_v.begin(),tmp_v.end());
+                    ans = toLongLong(tmp_v)
+                           + tmp * pow(k, nums.size()/2+1);
+                }                
+            }
+            else
+            {
+                int tmp = 0;
+                for(int i=0;i<nums.size()/2;i++)
+                {
+                    tmp = tmp*k+nums[i];
+                }
+                tmp = tmp+1;
+                vector<int> tmp_v = toVector(tmp, k);
+                reverse(tmp_v.begin(),tmp_v.end());
+                ans = toLongLong(tmp_v)
+                       + tmp * pow(k, nums.size()/2);
+            }
+        }
+        return ans;
+    }
+    bool isSymmetry(const vector<int>& a)
+    {
+        for (int i=0;i<a.size()/2;i++)
+        {
+            if(a[i]!=a[a.size()-1-i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<int> toVector(long long n, int k)
+    {
+        vector<int> ans;
+        if(n==0)
+        {
+            ans.push_back(0);
+            return ans;
+        }
+        
+        while(n!=0)
+        {
+            ans.push_back(n%k);
+            n=n/k;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    long long toLongLong(const vector<int>& a)
+    {
+        long long ans= 0;
+        for (int i=0;i<a.size();i++)
+        {
+            ans = ans*10+a[i];
+        }
+        return ans;
     }
 };
+int main()
+{
+    Solution s;
+    s.kMirror(2,5);
+    return 0;
+}
