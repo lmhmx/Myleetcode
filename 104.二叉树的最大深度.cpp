a102978ce6ab57acbb767dd7bef993f515af1009
lmhmx <liming18739796090@gmail.com>
2021-11-23 16:10:57 +0800

    迭代求深度

--- 104.二叉树的最大深度.cpp
+++ 104.二叉树的最大深度.cpp
@@ -10,23 +10,47 @@
 # include <queue>
 # include <algorithm>
 using namespace std;
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root==nullptr)
+        int ans = 0;
+        queue<TreeNode*> layer[2];
+        if(root!=nullptr)
         {
-            return 0;
+            layer[0].push(root);
         }
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        int cur;
+        int next;
+        TreeNode* p;
+        while(!layer[0].empty()||!layer[1].empty())
+        {
+            cur = layer[0].empty()?1:0;
+            next = 1-cur;
+            while(!layer[cur].empty())
+            {
+                p = layer[cur].front();
+                layer[cur].pop();
+                if(p->left!=nullptr)
+                {
+                    layer[next].push(p->left);
+                }
+                if(p->right!=nullptr)
+                {
+                    layer[next].push(p->right);
+                }
+            }
+            ans++;
+        }
+        return ans;
     }
 };
 // @lc code=end
