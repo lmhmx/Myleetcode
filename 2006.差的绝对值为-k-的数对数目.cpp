975b50c25c16dfd7b51cc2a1fb5d47ceec311eb3
lmhmx <liming18739796090@gmail.com>
2021-11-24 20:32:16 +0800

    差的绝对值为特定值的个数

--- 2006.差的绝对值为-k-的数对数目.cpp
+++ 2006.差的绝对值为-k-的数对数目.cpp
@@ -6,11 +6,27 @@
 
 // @lc code=start
 # include <vector>
+# include <algorithm>
+# include <unordered_map>
 using namespace std;
 class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
-
+        unordered_map<int, int> mm;
+        int ans = 0;
+        for (int i=0;i<nums.size();i++)
+        {
+            if(mm.find(nums[i]+k)!=mm.end())
+            {
+                ans = ans + mm[nums[i]+k];
+            }
+            if(mm.find(nums[i]-k)!=mm.end())
+            {
+                ans = ans + mm[nums[i]-k];
+            }
+            mm[nums[i]]++;
+        }
+        return ans;
     }
 };
 // @lc code=end
