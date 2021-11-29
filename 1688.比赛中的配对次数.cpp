23988b420f95fafb83b37d57ecb88b901e81bff5
lmhmx <liming18739796090@gmail.com>
2021-11-29 15:25:02 +0800

    比赛次数

---
+++ 1688.比赛中的配对次数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1688 lang=cpp
+ *
+ * [1688] 比赛中的配对次数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
+// @lc code=end
+
