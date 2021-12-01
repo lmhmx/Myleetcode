d31115d2e4d22c982e21c64bb11cf22da081cf1e
lmhmx <liming18739796090@gmail.com>
2021-12-01 21:05:28 +0800

    无重叠区间

---
+++ 435.无重叠区间.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=435 lang=cpp
+ *
+ * [435] 无重叠区间
+ */
+
+// @lc code=start
+#include <vector>
+# include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.empty()) {
+            return 0;
+        }
+        
+        sort(intervals.begin(), intervals.end(), [](const auto& u, const auto& v) {
+            return u[1] < v[1];
+        });
+
+        int n = intervals.size();
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            if (intervals[i][0] >= right) {
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n - ans;
+    }
+};
+
+// @lc code=end
+
